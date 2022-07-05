f29483b7e1394d9108009f93f5c7504323a61559
zenodinh <75799707+zenodinh@users.noreply.github.com>
2022-07-05 21:50:47 +0700

    Add plugin format

---
+++ src/Method/Method.ts
@@ -0,0 +1,17 @@
+export class Method {
+    path: string;
+    constructor(path: string){}
+
+    render(url: string) {
+        return url
+    }
+
+    getHighlightText() {
+        let highlight = "";
+        return highlight;
+    }
+
+    sendInfo() {
+        let highlight, expand, accountID, table
+    }
+}
---
+++ src/Method/PDF.ts
@@ -0,0 +1,21 @@
+import { Method } from "./Method";
+
+export class PDF extends Method {
+    path: string;
+    constructor(path: string){
+        super(path);
+    }
+
+    // render() {
+
+    // }
+
+    getHighlightText() {
+        let highlight = "";
+        return highlight;
+    }
+
+    sendInfo() {
+        let highlight, expand, accountID, table
+    }
+}
