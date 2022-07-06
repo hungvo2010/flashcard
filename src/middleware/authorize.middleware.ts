ec05ec6b1a526f859358e72919f8d06abbe4cdf2
zenodinh <75799707+zenodinh@users.noreply.github.com>
2022-07-06 17:17:15 +0700

    Fix

--- src/middleware/authorize.middleware.ts
+++ src/middleware/authorize.middleware.ts
@@ -5,31 +5,32 @@ import { Config } from 'src/constant/Config';
 
 @Injectable()
 export class AuthorizeMiddleware implements NestMiddleware {
-    private readonly dev: boolean = process.env.NODE_ENV !== 'production';
-    use(req: Request, res: Response, next: NextFunction) {
-        
-        let jwtToken: string;
-        if (req.header['Authorization'] && req.header['Authorization'].startsWith('Bearer')) {
-            jwtToken = req.header['Authorization'].split(' ')[1];
-        }
-        if (!jwtToken && req.cookies && req.cookies.jwt) {
-            jwtToken = req.cookies.jwt;
-        }
-        if (!jwtToken) {
-            return res.redirect('/signin');
-        }
-
-        try {
-            const JWT_SECRET = this.dev ? Config.JWT_SECRET_DEV : Config.JWT_SECRET_LIVE;
-            const userInfo = verify(jwtToken, JWT_SECRET);
-            req["user"] = userInfo;
-            next();
-        }
-
-        catch (err) {
-            return res.redirect('/signin');
-        }
-        next();
+  private readonly dev: boolean = process.env.NODE_ENV !== 'production';
+  use(req: Request, res: Response, next: NextFunction) {
+    let jwtToken: string;
+    if (
+      req.header['Authorization'] &&
+      req.header['Authorization'].startsWith('Bearer')
+    ) {
+      jwtToken = req.header['Authorization'].split(' ')[1];
     }
+    if (!jwtToken && req.cookies && req.cookies.jwt) {
+      jwtToken = req.cookies.jwt;
+    }
+    if (!jwtToken) {
+      return res.redirect('/signin');
+    }
+
+    try {
+      const JWT_SECRET = this.dev
+        ? Config.JWT_SECRET_DEV
+        : Config.JWT_SECRET_LIVE;
+      const userInfo = verify(jwtToken, JWT_SECRET);
+      req['user'] = userInfo;
+      next();
+    } catch (err) {
+      return res.redirect('/signin');
+    }
+    next();
   }
 }
