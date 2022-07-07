f43583c852045ceefe54030bd881e72a9401f553
hungvo2010 <hungvochanhdp1@gmail.com>
2022-07-07 10:06:03 +0700

    get all cards

--- src/controller/card.controller.ts
+++ src/controller/card.controller.ts
@@ -12,10 +12,11 @@ import { CardService } from '../service/card.service';
 import { Request, Response, NextFunction } from 'express';
 import { CartDto } from 'src/dto/card.dto';
 import { RCode } from 'src/constant/RCode';
+import { collection, getDocs, where } from 'firebase/firestore';
 
 @Controller('cards')
 export class CardController {
-  constructor(private cardService: CardService) {}
+  constructor(private cardService: CardService) { }
 
   @Post('create')
   async create(@Req() req: Request, @Res() res: Response) {
@@ -55,73 +56,63 @@ export class CardController {
     }
   }
 
-  @Post('/')
+  @Get('/')
   async getAll(@Req() req: Request, @Res() res: Response) {
-    let { table } = req.body;
-    console.log(table);
-    if (!table) {
+    let tables = await this.cardService.getTables(req["user"].userId);
+    let cardsResult = await this.cardService.getAllCardsOfTables(tables);
+    console.log(cardsResult);
+    res.render('card', {
+      cards: cardsResult,
+    })
+  }
+
+@Post(':id/update')
+async update(@Req() req: Request, @Res() res: Response) {
+  try {
+    let { id } = req.params;
+    let result = await this.cardService.update(id, req.body);
+    if (result == 1) {
       res.status(200).json({
-        status: 'No table',
-        result: 'failed',
-      });
-    } else {
-      let result = await this.cardService.getAll(table);
-      res.status(200).json({
-        status: 'Get all cards succesfully',
-        size: result.length,
-        cards: result,
+        status: `Update expand for card ${id} successfully`,
         result: 'success',
       });
-    }
-  }
-
-  @Post(':id/update')
-  async update(@Req() req: Request, @Res() res: Response) {
-    try {
-      let { id } = req.params;
-      let result = await this.cardService.update(id, req.body);
-      if (result == 1) {
-        res.status(200).json({
-          status: `Update expand for card ${id} successfully`,
-          result: 'success',
-        });
-      } else {
-        res.status(400).json({
-          status: `Update expand for card ${id} failed`,
-          result: 'failed',
-        });
-      }
-    } catch (error) {
+    } else {
       res.status(400).json({
-        status: 'Error while updating card',
-        message: error.message,
+        status: `Update expand for card ${id} failed`,
         result: 'failed',
       });
     }
+  } catch (error) {
+    res.status(400).json({
+      status: 'Error while updating card',
+      message: error.message,
+      result: 'failed',
+    });
   }
+}
 
-  @Post(':id/delete')
-  async delete(@Req() req: Request, @Res() res: Response) {
-    try {
-      let { id } = req.params;
-      let result = await this.cardService.delete(id);
-      if (result == 1) {
-        res.status(200).json({
-          status: `Delete expand for card ${id} successfully`,
-          result: 'success',
-        });
-      } else {
-        res.status(400).json({
-          status: `Delete expand for card ${id} failed`,
-          result: 'failed',
-        });
-      }
-    } catch (error) {
+@Post(':id/delete')
+async delete (@Req() req: Request, @Res() res: Response) {
+  try {
+    let { id } = req.params;
+    let result = await this.cardService.delete(id);
+    if (result == 1) {
+      res.status(200).json({
+        status: `Delete expand for card ${id} successfully`,
+        result: 'success',
+      });
+    } else {
       res.status(400).json({
-        status: `Delete expand for card failed`,
+        status: `Delete expand for card ${id} failed`,
         result: 'failed',
-        message: error.message
       });
     }
+  } catch (error) {
+    res.status(400).json({
+      status: `Delete expand for card failed`,
+      result: 'failed',
+      message: error.message
+    });
   }
+}
 }
